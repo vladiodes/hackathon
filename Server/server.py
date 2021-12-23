24f05e0860c2c9ffc4c55c99e414286f397bb3c9
michael zaslavski <michael.za6@gmail.com>
2021-12-23 15:43:31 +0200

    decided the winner, need to synchronize threads

--- Server/server.py
+++ Server/server.py
@@ -13,13 +13,27 @@ def create_math_problems():
     return exercises, answers
 
 
+def play(player_socket, expected_answer, group_number, opponent_group):  # need to synchronize the threads
+
+    global_vars = globals()
+
+    global stop_threads
+    while not stop_threads:
+        answer = player_socket.recv(4).decode()
+        global_vars['stop_threads'] = True
+        if answer == expected_answer:
+            player_socket.send(group_number.tobytes(4, 'big'))
+        else:
+            player_socket.send(opponent_group.tobytes(4, 'big'))
+
+
 class Server:
 
     def __init__(self):
         self.name = socket.gethostname()
         self.ip = socket.gethostbyname(self.name)
 
-    def run_udp(self, tcp_socket_port, stop):
+    def run_udp(self, tcp_socket_port):
         print('Server started, listening on IP address {ip}'.format(ip=self.ip))
 
         # make the UDP message according to the format
@@ -31,11 +45,14 @@ class Server:
         server_UDP_socket.bind(('', 0))
 
         # send offer messages
-        while True:
+        global stop_threads
+        while not stop_threads:
             time.sleep(1)
             global stop_threads
             if not stop_threads:
                 server_UDP_socket.sendto(message, ('255.255.255.255', 13117))
+            else:
+                server_UDP_socket.close()
 
     def run_server(self):
 
@@ -48,7 +65,7 @@ class Server:
 
         # run the thread that will send offer messages over UDP while listening for connection requests over TCP
         global_vars['stop_threads'] = False
-        udp_thread = threading.Thread(target=self.run_udp, args=(server_TCP_socket_port, lambda: stop_threads,))
+        udp_thread = threading.Thread(target=self.run_udp, args=server_TCP_socket_port)
         udp_thread.start()
 
         # listen for connection requests
@@ -65,6 +82,7 @@ class Server:
                 player2_socket = connectionSocket
 
         # 2 players have connected , stop accepting new players and stop sending offers
+        server_TCP_socket.close()
         global_vars['stop_threads'] = True
         time.sleep(10)
 
@@ -74,7 +92,7 @@ class Server:
         exercises, answers = create_math_problems()
         exercise_number = random.randrange(0, len(exercises))
         exercise = exercises[exercise_number]
-        answer = answers[exercise_number]
+        exercise_answer = answers[exercise_number]
         message_txt = "Welcome to Quick Maths\n" \
                       "Player 1: {group1:group1_name}\n" \
                       "Player 2: {group2:group2_name}\n" \
@@ -82,30 +100,49 @@ class Server:
                       "Please answer the following question as fast as you can\n" \
                       "How much is {question:random_exercise}?"
         message = message_txt.format(group1=group1_name, group2=group2_name, question=exercise)
+
+        # make "Game Over" message
+        game_over_txt = "Game Over!\n" \
+                        "The correct answer was {answer:ans}!\n" \
+                        "Congratulations to the winner: {winning_group:name}"
+        draw_txt = "Game Over!\n" \
+                   "The correct answer was {answer:ans}!\n" \
+                   "The game ended in a draw"
+        msg = None
+
+        # send welcome messages
         player1_socket.send(message.encode())
         player2_socket.send(message.encode())
 
         # prepare threads for clients
         global_vars['stop_threads'] = False
-        player1_thread = threading.Thread(target=self.play, args=(player1_socket, answer, 1, 2))
-        player2_thread = threading.Thread(target=self.play, args=(player2_socket, answer, 2, 1))
+        player1_thread = threading.Thread(target=play, args=(player1_socket, exercise_answer, 1, 2))
+        player2_thread = threading.Thread(target=play, args=(player2_socket, exercise_answer, 2, 1))
 
         # start the threads
         player1_thread.start()
         player2_thread.start()
 
-        #now we decide the winner
+        # now we decide the winner
+        try:
+            threading.Condition.wait_for(lambda: global_vars['stop_threads'], timeout=10)
+            # someone has written , try reading from player 1 then from player 2
+            player1_socket.settimeout(0)
+            player2_socket.settimeout(0)
+            try:
+                winner = player1_socket.recv(4).decode()
+                # create message
+                msg = game_over_txt.format(answer=exercise_answer, winning_group=winner)
+            except TimeoutError:
+                winner = player2_socket.recv(4).decode()
+                # create message
+                msg = game_over_txt.format(answer=exercise_answer, winning_group=winner)
+        except TimeoutError:
+            # create draw message
+            msg = draw_txt
 
-    def play(self, player_socket, expected_answer, group_number, opponent_group):
+        player1_socket.settimeout(None)
+        player2_socket.settimeout(None)
 
-        global_vars = globals()
-
-        while True:
-            answer = player_socket.recv(4).decode()
-            global stop_threads
-            if not stop_threads:
-                global_vars['stop_threads'] = True
-                if answer == expected_answer:
-                    player_socket.send(group_number.tobytes(4, 'big'))
-                else:
-                    player_socket.send(opponent_group.tobytes(4, 'big'))
+        player1_socket.send(msg)
+        player2_socket.send(msg)
