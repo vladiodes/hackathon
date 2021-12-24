5348a0ffede0d0b65b9c9aa555b66560b1572016
vladiodes@gmail.com <vladiodes@gmail.com>
2021-12-24 16:01:06 -0500

    Fixing errors, next: debug on ssh

--- Client/client.py
+++ Client/client.py
@@ -10,6 +10,7 @@ udp_port = 13117
 team_name = "Nice name"
 magic_cookie = 0xabcddcba
 offer_op_code = 0x2
+timeout_interval = 10
 
 def acceptOffer():
     """
@@ -23,7 +24,7 @@ def acceptOffer():
     msg_tuple = struct.unpack('IbH',incoming_msg) #I = unsigned int, 4 bytes magic cookie, b = byte of offer msg, H = unsigned short, 2 bytes representing server port
     if msg_tuple[0] != magic_cookie or msg_tuple[1] != offer_op_code:
         return False
-    udp_sock.close
+    udp_sock.close()
     return (server_ip_address[0],msg_tuple[2]) #returning server's ip and server's tcp port  
 
 def handleTCP(server_ip,server_port):
@@ -31,14 +32,18 @@ def handleTCP(server_ip,server_port):
     Creates a tcp connection with the server and returns the tcp socket for the game
     """
     tcp_sock = socket(AF_INET, SOCK_STREAM)
-    tcp_sock.connect((server_ip,server_port))
-    tcp_sock.send((team_name + "\n").encode())
+    try:
+        tcp_sock.connect((server_ip,server_port))
+        tcp_sock.send((team_name + "\n").encode())
+    except:
+        tcp_sock.close()
+        return False
     return tcp_sock
 
 def read_from_stdin(tcp_socket):
-    reads,_,_ = select.select([sys.stdin,[],[],10])
+    reads,_,_ = select.select([sys.stdin],[],[],timeout_interval)
     if sys.stdin in reads:
-        ans=sys.stdin.read(1)
+        ans=sys.stdin.readline()[0]
         try:
             tcp_socket.send(ans.encode())
         except:
@@ -48,15 +53,27 @@ def gameMode(tcp_sock):
     """
     This function simulates the game via the tcp connection with the server
     """
-    welcome_msg = tcp_sock.recv(buf_size).decode()
-    print(welcome_msg)
+    try:
+        welcome_msg = tcp_sock.recv(buf_size).decode()
+        print(welcome_msg)
+    except:
+        tcp_sock.close()
+        return
+    
     
 
     stdin_thread = threading.Thread(target=read_from_stdin,args=(tcp_sock, ))
     stdin_thread.start()
-    response = tcp_sock.recv(buf_size).decode()
+    try:
+        response = tcp_sock.recv(buf_size).decode()
+        print(response)
+    except:
+        tcp_sock.close()
+        return
+
+    stdin_thread.join()
+    tcp_sock.close()
     
-    print(response)
 
 
 
@@ -70,5 +87,6 @@ while 1:
     if server_offer != False:
         print("Received offer from " + server_offer[0] + ", attempting to connect...")
         tcp_sock = handleTCP(server_offer[0],server_offer[1])
-        gameMode(tcp_sock)
-        is_first_cycle = False
+        if tcp_sock!=False:
+            gameMode(tcp_sock)
+            is_first_cycle = False
